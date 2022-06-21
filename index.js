050306695f9a87667456926a03aad8605b273707
daydinn <daydinn98@gmail.com>
2022-06-22 01:14:34 +0200

    gsap animations+video
    gsap animations+video

--- index.js
+++ index.js
@@ -1,15 +1,97 @@
+
+
 $(document).ready(function () {
 
-    $('.first-button').on('click', function () {
+  $('.first-button').on('click', function () {
+
+    $('.animated-icon1').toggleClass('open');
+  });
+  $('.second-button').on('click', function () {
+
+    $('.animated-icon2').toggleClass('open');
+  });
+  $('.third-button').on('click', function () {
+
+    $('.animated-icon3').toggleClass('open');
+  });
+});
+
+
+
+
+
+/*
+function isElementInViewport(el){
+if(typeof jQuery=== "function" && el instanceof jQuery){
+el = el[0];
+
+}
+var rect = el.getBoundingClientRect();
+return(
+(rect.top <= 0
+&& rect.bottom >= 0)
+||
+(rect.bottom >= (window.innerHeight || document.documentElement.clientHeight)&&
+rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+||
+(rect.top >= 0 &&
+  rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+  var scroll = window.requestAnimationFrame ||
+function(callback) {window.setTimeout(callback,1000/60)};
+
+var elementsToShow = document.querySelectorAll('.show-on-scroll'
+); //look for all elements with class show-on-scroll and return them as a array
+
+
+function loop(){ //look throught all elements and check if they are visiable
+elementsToShow.forEach(function (element){
+if (isElementInViewport(element)){
+element.classList.add('is-visible');
+}else{  //otherwise remove the class
+element.classList.remove('is-visible');
+}
+});
+scroll(loop); //pass this function as callback(it will repeat)
+}
+
+loop();
+
+/*
+window.addEventListener('scroll', function(){
+  console.log("Scrollin'");
+
+  })
+  */
+
+
   
-      $('.animated-icon1').toggleClass('open');
-    });
-    $('.second-button').on('click', function () {
   
-      $('.animated-icon2').toggleClass('open');
-    });
-    $('.third-button').on('click', function () {
   
-      $('.animated-icon3').toggleClass('open');
-    });
+  
+  
+  
+  
+  
+  
+  
+  window.addEventListener('click', () => {
+ document.getElementById("song").play();
+
   });
--- try.js
+++
@@ -1,3 +0,0 @@
-$('i').click(function(){
-    $('.content').toggleClass('slide');
-});
